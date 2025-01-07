24e32e0ad57c377b5f436b11826d8d81405ce7c3
Janecres <janecresdalubatan@gmail.com>
2025-01-07 18:59:37 +0800

    Add files via upload

---
+++ Simpleproject.java
@@ -0,0 +1,20 @@
+
+public class Simpleproject {
+    public static void main(String[] args) {
+     
+     String userName = "Janecres";
+     int passWord = 12345;
+     
+     //Log in processing
+     if (userName == "Janecres" && passWord == 12345){
+        System.out.println("Log in success");
+
+     }else{
+     System.out.println("Invalid login");
+     }
+     
+     }
+     }
+    
+    
+
